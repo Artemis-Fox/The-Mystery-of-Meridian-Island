d85ff2ec8868246ba46e75d73e074a68d9120951
Artemis-Fox <105129173+Artemis-Fox@users.noreply.github.com>
2023-07-25 19:24:28 -0600

    Add files via upload

--- TextAdv.py
+++ TextAdv.py
@@ -1,5 +1,6 @@
 import math, random
 import time
+import sys
 
 GameLoop = True
 
@@ -13,36 +14,39 @@ class Room:
         self.InRoom = False
 class Tool:
     def __init__(self, name):
-        self.name
+        self.name = name
 
 
 Inventory = []
 
-while GameLoop == True:
-    print(""" 
-    ==================================================
-            The Mystery of Meridian Island
-    ==================================================
-    """)
 
-    StartGame = False 
+print(""" 
+==================================================
+        The Mystery of Meridian Island
+==================================================
+""")
+
+StartGame = True
+while StartGame == True:
     StartScreen = input("Are you ready to brave Meridian Island? Y, N\n")
-    while StartGame == False:
-        if StartScreen == "Y" or "y":
-            continue
-        elif StartScreen == "N" or "n":
-            print("Then begone with ye! and only return when ya got the stones")
-            GameLoop = False
-        else:
-            print("That was an invalid input matey, try again!")
+
+    if StartScreen == "y" or StartScreen == "Y":
+        break
+    elif StartScreen == "n" or StartScreen == "N":
+        sys.exit("Then Begone with ye! and only return when you got the stones!")
+    else:
+        continue
+
+while GameLoop == True:
+
 
     Name = input("Who are you?: \n")
-    print("\n")
+    print("\n:")
 
     beach = Room("Beach", ["1", "2", "3"], "A beach that you washed up on", True)
-    Hardwin = Tool("hardwin")
+    Hardwin = Tool("Hardwin")
 
-    if beach.InRoom == True:
+    while beach.InRoom == True:
         print(Name + ", You wake up face down, the feel of sand on your face and the sound of the waves close by. \n Lifting yourself up, you are on a beach. Behind you the ocean stretches past the horizon with the remains of a ship bobbing up and down with the waves\n")
         print("You start to remember what happened, you where on that ship. It was a Carrack, a large vessle of 150 feet (45 meters) in length with a crew of 40 men. Her name was EV Pathfinder, and it seems you are the only surviver of her crash.\n")
         print("Suddenly, a voice calls out to you! looking in the direction you see a skull with a single golden tooth.\n")
